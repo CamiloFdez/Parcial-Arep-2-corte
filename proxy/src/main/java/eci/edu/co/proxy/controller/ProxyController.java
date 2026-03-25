41f893005ead892e429163253233a6905a2027df
CamiloFdez <camilo.fernandez-d@mail.escuelaing.edu.co>
2026-03-25 12:26:50 -0500

    iniciando controlador del proxy

--- proxy/src/main/java/eci/edu/co/proxy/controller/ProxyController.java
+++ proxy/src/main/java/eci/edu/co/proxy/controller/ProxyController.java
@@ -1,6 +1,54 @@
 package eci.edu.co.proxy.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProxyController {
-    
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "http://localhost:8080/collatzsequence?value=";
+
+    @GetMapping(value = "/collatzsequence", produces = "application/json")
+    public String collatzsequence(@RequestParam(value = "value") int value) throws IOException {
+        return makeRequest(value, "GET");
+    }
+
+    @PostMapping(value = "/collatzsequence", produces = "application/json")
+    public String collatzsequencePost(@RequestParam(value = "value") int value) throws IOException {
+        return makeRequest(value, "POST");
+    }
+
+    private String makeRequest(int value, String method) throws IOException {
+        URL obj = new URL(GET_URL + value);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod(method);
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println(method + " Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            String response = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                response += inputLine;
+            }
+            in.close();
+
+            return response;
+        } else {
+            return "{\"operation\":\"error\",\"input\":" + value + ",\"output\":\"" + method + " request not worked\"}";
+        }
+    }
 }
---
+++ proxy/src/main/java/eci/edu/co/proxy/controller/Response.java
@@ -0,0 +1,3 @@
+package eci.edu.co.proxy.controller;
+
+public record Response(String operation, int input, String output) { }
