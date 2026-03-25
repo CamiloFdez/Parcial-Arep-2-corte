def9a7454aa2d42de3952bc6d623b98108dcd62c
Camilo Andres Fernandez Diaz <158209960+CamiloFdez@users.noreply.github.com>
2026-03-25 11:46:41 -0500

    subiendo controlador

--- mathcollatz/src/main/java/eci/edu/co/mathcollatz/controller/MathController.java
+++ mathcollatz/src/main/java/eci/edu/co/mathcollatz/controller/MathController.java
@@ -1,7 +1,24 @@
-package eci.edu.co.mathcollatz.controller;
+package eci.edu.co.mathcollatz;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class CollatzController {
 
-public class MathController {
-    
+    private final CollatzService collatzService = new CollatzService();
+
+    @GetMapping("/collatzsequence")
+    public Response collatzsequence(@RequestParam(value = "value") int value) {
+        String sequence = collatzService.getCollatzSequence(value);
+        return new Response("collatzsequence", value, sequence);
+    }
+
+    @PostMapping("/collatzsequence")
+    public Response collatzsequencePost(@RequestParam(value = "value") int value) {
+        String sequence = collatzService.getCollatzSequence(value);
+        return new Response("collatzsequence", value, sequence);
+    }
 }
